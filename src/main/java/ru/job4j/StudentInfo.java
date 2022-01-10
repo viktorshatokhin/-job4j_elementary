b3475ce4d8e2b418964c7579beb7caf0b79e7726
viktorshatokhin <viktorsh842@gmal.com>
2022-01-10 22:34:38 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Viktor Shatokhin");
+        System.out.println("06.07.2020");
+    }
+}
