9ab17f82eca563261e6b459ceab5bd7c05b96b43
viktorshatokhin <viktorsh842@gmail.ru>
2022-01-12 23:10:22 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Multiply.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class Multiply {
-    public static void main(String[] args){
-        System.out.println("1 * 2 = 2");
-        System.out.println("1 * 3 = 3");
-        System.out.println("1 * 9 = 9");
-    }
-}
